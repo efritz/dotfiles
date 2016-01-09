94e5bdc47442272581a5f042602cec2e7a4617ef
Eric Fritz <eric@eric-fritz.com>
2016-01-08 23:11:27 -0600

    Add update script.

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r bin ~/bin
+cp -r shell ~/.shell
+cp bash_profile ~/.bash_profile
+cp Sublime/User/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+
+source ~/.bash_profile
