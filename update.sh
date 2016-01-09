56301baba9361b13c0780732b87873c382959d93
Eric Fritz <eric@eric-fritz.com>
2016-01-08 23:13:35 -0600

    Rename {shell}->{bash}.

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp -r bin ~/bin
-cp -r shell ~/.shell
+cp -r bash ~/.shell
 cp bash_profile ~/.bash_profile
 cp Sublime/User/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 
