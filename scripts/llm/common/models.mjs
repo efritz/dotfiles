c34cb795fd42f7f2ea8498d155af08399c3c2d05
Eric Fritz <eric@eric-fritz.com>
2024-06-20 09:55:57 -0500

    Update sonnet to 3.5.

--- scripts/llm/common/models.mjs
+++ scripts/llm/common/models.mjs
@@ -2,7 +2,7 @@ export const models = {
     'gpt-4o': { provider: 'OpenAI',    model: 'gpt-4o' },
     'gpt-4':  { provider: 'OpenAI',    model: 'gpt-4'  },
     'haiku':  { provider: 'Anthropic', model: 'claude-3-haiku-20240307'  },
-    'sonnet': { provider: 'Anthropic', model: 'claude-3-sonnet-20240229' },
+    'sonnet': { provider: 'Anthropic', model: 'claude-3-5-sonnet-20240620' },
     'opus':   { provider: 'Anthropic', model: 'claude-3-opus-20240229'   },
 }
 
