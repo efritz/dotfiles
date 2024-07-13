ce51b3c60a95b62d96f4612f9e7de4de20ec2660
Eric Fritz <eric@eric-fritz.com>
2024-07-13 17:01:00 -0500

    Update comments.

--- ai/scripts/internal/completions.mjs
+++ ai/scripts/internal/completions.mjs
@@ -5,13 +5,17 @@ const commands = commandDescriptions.map(({ prefix, args }) => prefix + (args ? 
 
 export function completer(line) {
     if (line.startsWith(':load')) {
-        // Expand files matching the last entry in this sequence
+        // Expand files for loading that expand the last entry of the
+        // current list of files the user has provided. If a user has
+        // added a space to the previous file, we consider that "done"
+        // and won't suggest further expansion.
+
         const parts = line.split(' ');
         const lastEntry = parts[parts.length - 1];
         return [expandFilePath(lastEntry), lastEntry];
     }
 
-    // Show all completions if none found
+    // Complete any meta command; if the line is empty show all meta commands.
     const hits = commands.filter(completion => completion.startsWith(line));
     return [hits.length ? hits : commands, line];
 }
