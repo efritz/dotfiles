a2b99984ba4399ae0545d5ce561e99138866430e
Eric Fritz <eric@eric-fritz.com>
2024-08-15 16:29:01 -0500

    Move AI scripts to new directory.

--- ai/scripts/src/tools/context.ts
+++
@@ -1,9 +0,0 @@
-import { Provider } from '../providers/provider'
-import { InterruptHandler } from '../util/interrupts/interrupts'
-import { Prompter } from '../util/prompter/prompter'
-
-export type ExecutionContext = {
-    interruptHandler: InterruptHandler
-    prompter: Prompter
-    provider: Provider
-}
