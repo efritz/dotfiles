cbc23f351e7a11f8c95bc18ea10bc305f837b0c3
Eric Fritz <eric@eric-fritz.com>
2025-02-19 10:31:07 -0600

    Remove macos limits script.

--- macos/set-limits.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-cd "$(dirname "${BASH_SOURCE[0]}")"/..
-
-for basename in 'maxfiles' 'maxproc'; do
-    cp "./macos/limit.${basename}.plist" /Library/LaunchDaemons
-    chown root:wheel "/Library/LaunchDaemons/limit.${basename}.plist"
-    launchctl load -w "/Library/LaunchDaemons/limit.${basename}.plist"
-done
